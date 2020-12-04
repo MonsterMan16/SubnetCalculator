5cc5bcd35a312053305fbaa2460bdcc87f21e84b
Keelan McCarthy <mccarthykeelan@gmail.com>
2020-12-04 01:18:56 +0000

    IP Methods updated 04/12/2020 1:18am - Added integerToEightBitBinary Method

--- Networking/TestIPMethods.java
+++ Networking/TestIPMethods.java
@@ -4,12 +4,18 @@ import javax.swing.*;
 
 public class TestIPMethods {
     public static void main(String[] args) {
-        String ipAddress = JOptionPane.showInputDialog(null, "Input an IP Address", "IP Input", JOptionPane.QUESTION_MESSAGE);
-        if(IP.isValid(ipAddress)) {
-            JOptionPane.showMessageDialog(null,"IP Address: " + ipAddress + " is a valid IP", "IP Address Valid", JOptionPane.INFORMATION_MESSAGE);
-        } else
-        {
-            JOptionPane.showMessageDialog(null,"IP Address: " + ipAddress + " is not a valid IP","IP Validation Error", JOptionPane.ERROR_MESSAGE);
-        }
+//        String ipAddress = JOptionPane.showInputDialog(null, "Input an IP Address", "IP Input", JOptionPane.QUESTION_MESSAGE);
+//        if(IP.isValid(ipAddress)) {
+//            JOptionPane.showMessageDialog(null,"IP Address: " + ipAddress + " is a valid IP", "IP Address Valid", JOptionPane.INFORMATION_MESSAGE);
+//        } else
+//        {
+//            JOptionPane.showMessageDialog(null,"IP Address: " + ipAddress + " is not a valid IP","IP Validation Error", JOptionPane.ERROR_MESSAGE);
+//        }
+
+        int num = Integer.parseInt(JOptionPane.showInputDialog(null, "Input a number", "IP Input", JOptionPane.QUESTION_MESSAGE));
+
+        JOptionPane.showMessageDialog(null,"Integer in 8Bit Binary form is " + IP.integerToEightBitBinary(num), "IP Address Valid", JOptionPane.INFORMATION_MESSAGE);
+
+
     }
 }
