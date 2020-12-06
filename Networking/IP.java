edc2780cea476913574de482db1eaa890fb7e51e
Keelan McCarthy <mccarthykeelan@gmail.com>
2020-12-06 15:49:51 +0000

    IP Methods updated 04/12/2020 1:18am - Created IPString to array method

--- Networking/IP.java
+++ Networking/IP.java
@@ -2,24 +2,35 @@ package Networking;
 
 import java.util.regex.*;
 
+
 public final class IP {
     public static final int TYPE_SUBNET = 0;
     public static final int TYPE_IP = 1;
+    private static final String IPV4_REGEX_OCTET = "(\\d{1,3})";
+    private static final String IPV4_REGEX = "^" + IPV4_REGEX_OCTET + IPV4_REGEX_OCTET + IPV4_REGEX_OCTET + "(\\d{1,3})$";
 
     public static boolean isValid(String ipAddress) {
-        String regexExpression = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])" + "\\." + "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])" + "\\." +
-                "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])" + "\\." + "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-4])";
-        Pattern regexCompiled = Pattern.compile(regexExpression);
+        Pattern compiledRegex = Pattern.compile(IPV4_REGEX);
         // Using the matcher method provided by the regex package
-        Matcher check = regexCompiled.matcher(ipAddress);
+        Matcher check = compiledRegex.matcher(ipAddress);
         boolean match = check.matches();
         return match;
     }
 
-//    public static int[] addressToArray(String ip_address) {
-//
-//    }
-//
+    public static int[] addressToArray(String ip_address) {
+        int[] addressArray = new int[4];
+          for(int i= 0; i < addressArray.length; i++) {
+              if(i == 3) {
+                  addressArray[i] = Integer.parseInt(ip_address.substring(0,ip_address.length()));
+                  continue;
+              }
+              addressArray[i] = Integer.parseInt(ip_address.substring(0,ip_address.indexOf(".")));
+              ip_address = ip_address.substring((ip_address.indexOf(".")+1),ip_address.length());
+//              ip_address = ip_address.replace((IPV4_REGEX_OCTET + "\\."),"");
+          }
+        return addressArray;
+    }
+
 //    public static int[] integerArrayToBinaryArray(int[] ip_address) {
 //
 //    }
--- Networking/TestIPMethods.java
+++ Networking/TestIPMethods.java
@@ -1,6 +1,7 @@
 package Networking;
 
 import javax.swing.*;
+import java.util.Arrays;
 
 public class TestIPMethods {
     public static void main(String[] args) {
@@ -12,15 +13,13 @@ public class TestIPMethods {
 //            JOptionPane.showMessageDialog(null,"IP Address: " + ipAddress + " is not a valid IP","IP Validation Error", JOptionPane.ERROR_MESSAGE);
 //        }
 
-        int num = Integer.parseInt(JOptionPane.showInputDialog(null, "Input a number", "IP Input", JOptionPane.QUESTION_MESSAGE));
-
-        JOptionPane.showMessageDialog(null,"Integer in 8Bit Binary form is " + intToBinary(num), "IP Address Valid", JOptionPane.INFORMATION_MESSAGE);
-
-
-    }
-
-    public static String intToBinary(int n)
-    {
+//        int num = Integer.parseInt(JOptionPane.showInputDialog(null, "Input a number", "IP Input", JOptionPane.QUESTION_MESSAGE));
+//
+//        JOptionPane.showMessageDialog(null,"Integer in 8Bit Binary form is " + intToBinary(num), "IP Address Valid", JOptionPane.INFORMATION_MESSAGE);
+        String ip = "192.168.1.254";
+        int[] ipArray = IP.addressToArray(ip);
+        System.out.println(Arrays.toString(ipArray));
 
     }
+
 }
